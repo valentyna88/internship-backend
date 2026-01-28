32b4d5eea7cbbaabcec2a53dd0ff988d9dae5a73
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2026-01-28 21:04:04 +0200

    refactor: extract healthcheck response to service and use HttpStatus

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,17 +4,8 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('hello')
-  getHello(): string {
-    return this.appService.getHello();
-  }
   @Get()
   healthcheck() {
-    return {
-      status_code: 200,
-      detail: 'ok',
-      result: 'working',
-    };
+    return this.appService.getHealthcheck();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealthcheck() {
+    return {
+      status_code: HttpStatus.OK,
+      detail: 'ok',
+      result: 'working',
+    };
   }
 }

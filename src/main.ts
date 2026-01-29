23fdb19b01191a589b70275a1ef248d1f32ab5cc
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2026-01-29 22:24:23 +0200

    feat: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

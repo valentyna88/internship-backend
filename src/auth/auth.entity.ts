fb481c6571ff8723d3114ce649e2578a4cf764d4
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2026-02-02 13:57:06 +0200

    feat: add User and Auth entities

---
+++ src/auth/auth.entity.ts
@@ -0,0 +1,23 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
+import { User } from '../user/user.entity';
+
+@Entity('auth')
+export class Auth {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  user: User;
+
+  @Column()
+  refreshTokenHash: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}

030e0ed249a06baf209f20d49e2fb8a644016db2
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2026-02-07 22:46:46 +0200

    docs(api): add Swagger decorators for user and common responses

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,7 +11,12 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { ApiResponseDto } from '../common/dto/api-response.dto';
 import { UserPaginationDetailDto } from './dto/user-pagination.dto';
 import { UserSingleDetailDto } from './dto/user-detail.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -21,26 +26,36 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiOkResponse({ type: UserPaginationDetailDto })
   async findAll(@Query() pagination: PaginationDto) {
     return await this.userService.findAll(pagination);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Getting user by id' })
+  @ApiOkResponse({ type: UserSingleDetailDto })
   async findOne(@Param('id') id: string) {
     return await this.userService.findOne(id);
   }
 
   @Post('signup')
+  @ApiOperation({ summary: 'Creating a new user' })
+  @ApiCreatedResponse({ type: ApiResponseDto })
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.userService.create(createUserDto);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Updating an existing user' })
+  @ApiOkResponse({ type: UserSingleDetailDto })
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return await this.userService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Deleting an existing user' })
+  @ApiOkResponse({ type: ApiResponseDto })
   async remove(@Param('id') id: string) {
     return await this.userService.remove(id);
   }
